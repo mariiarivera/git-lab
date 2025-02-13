016747ec153ef465fcbabce806395f9b9c8f1171
mariiarivera <a01029678@tec.mx>
2025-02-13 14:58:05 -0600

    Added the file prueba_terminal, with a folder inside a message in cpp, and a file called script

---
+++ prueba_terminal/nextarchivo/mensaje.js
@@ -0,0 +1,1 @@
+console.log("Hola profe, esta parte si la acabe. solo me falta la ultima parte de la cuarta. espero que si este bien. te veo la siguiente semana. byeee"
---
+++ prueba_terminal/script.js
@@ -0,0 +1,2 @@
+console.log("Hello World")
+// Asi me aparecio el merge conflict
